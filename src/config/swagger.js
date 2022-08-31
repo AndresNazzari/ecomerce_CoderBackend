81e010999ea46d2bd1179419d7a02fb7973e609e
Andres Nazzari <lordcliff@gmail.com>
2022-08-30 23:41:21 -0300

    📝 DOC: fixed swagger apis

--- src/config/swagger.js
+++ src/config/swagger.js
@@ -4,9 +4,9 @@ const options = {
   definition: {
     openapi: '3.0.0',
     info: { title: 'API Documentation', description: 'Simple CRUD API', version: '1.0.0' },
-    servers: [{ url: '/api/v1/' }],
+    servers: [{ url: 'http://localhost:3000/api/v1/' }],
   },
-  apis: ['./src/routes/api/v1/*.js'],
+  apis: ['./src/routes/api/v1/docs/*.js'],
 };
 
 export default swaggerJSDoc(options);
