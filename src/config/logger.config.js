9da85f25bc315d7c804677e3420b6689cb8d6050
Andres Nazzari <lordcliff@gmail.com>
2022-10-26 20:16:53 +0900

    🚑 HOTFIX:

--- src/config/logger.config.js
+++ src/config/logger.config.js
@@ -1,17 +1,17 @@
 import log4js from 'log4js';
 
 log4js.configure({
-    appenders: {
-        default: { type: 'console' },
-        infoFile: { type: 'file', filename: './logs/info.log' },
-        warnFile: { type: 'file', filename: './logs/warn.log' },
-        errorFile: { type: 'file', filename: './logs/error.log' },
-    },
-    categories: {
-        default: { appenders: ['default', 'infoFile'], level: 'info' },
-        warnFile: { appenders: ['default', 'warnFile'], level: 'warn' },
-        errorFile: { appenders: ['default', 'errorFile'], level: 'error' },
-    },
+  appenders: {
+    default: { type: 'console' },
+    infoFile: { type: 'file', filename: './logs/info.log' },
+    warnFile: { type: 'file', filename: './logs/warn.log' },
+    errorFile: { type: 'file', filename: './logs/error.log' },
+  },
+  categories: {
+    default: { appenders: ['default', 'infoFile'], level: 'info' },
+    warnFile: { appenders: ['default', 'warnFile'], level: 'warn' },
+    errorFile: { appenders: ['default', 'errorFile'], level: 'error' },
+  },
 });
 
 const loggerDefault = log4js.getLogger();
